84c1f40b00e8d76d82f03630af11ffbcf53d2e57
Jamal Jabbar <jamal.jabbar10@gmail.com>
2026-05-28 16:09:06 -0500

    updated next config for deploy

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: "/KreamProxies",
+  assetPrefix: "/KreamProxies/",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   turbopack: {
     root: process.cwd()
   }
