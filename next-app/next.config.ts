a28749f7c160d5990463b694a5b9512cd107a123
Jamal Jabbar <jamal.jabbar10@gmail.com>
2026-05-28 16:27:53 -0500

    added isProd flag

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: "/KreamProxies",
-  assetPrefix: "/KreamProxies/",
+  output: "export",
+  basePath: isProd ? "/KreamProxies" : "",
+  assetPrefix: isProd ? "/KreamProxies/" : "",
   images: {
     unoptimized: true,
   },
